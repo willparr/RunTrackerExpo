7f7b165212b6d63890fd5eb76c006e449b0c1e37
willparr <will@teamsoftware.com>
2020-12-19 12:11:56 -0600

    adding statbox and stat component

--- components/StatBox.tsx
+++ components/StatBox.tsx
@@ -1,35 +1,53 @@
 import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
-import { StyleSheet, TouchableOpacity } from 'react-native';
-import { Card } from 'react-native-elements';
+import { StyleSheet, TextStyle, TouchableOpacity } from 'react-native';
+import { Card, Text} from 'react-native-elements';
 import CenterView from '../storybook/stories/CenterView'
 
-import { Text, View } from './Themed';
+import { View } from './Themed';
 
+type Size = 'small' | 'medium' | 'large' | 'xlarge'
+type Weight = 'normal' | 'bold' | 'light' | 'bolder'
 interface StatBoxProps {
     statName: string
     stat: string | number
     children?: React.ReactNode
+    statSize?: Size
+    statWeight?: Weight
+    labelWeight?: Weight
+    labelSize?: Size
+}
 
+const textSize: Record<Size, number> = {
+    'small': 22,
+    'medium': 34,
+    'large': 64,
+    'xlarge': 80
+}
+
+const textWeight: Record<Weight, TextStyle['fontWeight']> = {
+    'normal': 'normal',
+    'bold': 'bold',
+    'light': '100',
+    'bolder': '700'
 }
 
 
 export default function StatBox(props: StatBoxProps) {
-    const {statName, stat} = props
+    const {statName, stat, statSize, statWeight, labelSize, labelWeight} = props
+    console.debug(statName)
+    console.debug(stat)
     return (
-        <View style={{flex: 1, borderColor: 'blue',
-        borderWidth: 1}}>
-            <Card wrapperStyle={{alignContent: 'center'}}>
-                <Card.Title>
-                    <Text>{statName}</Text>
-                </Card.Title >
-                <CenterView>
-                    <Text>
-                        {stat}
+            <Card containerStyle={{alignContent: 'center', margin: 0}}>
+                <View style={{alignItems: 'center'}}>
+                    <Text style={{fontSize: textSize[statSize ?? 'medium'], fontWeight: textWeight[statWeight ?? 'normal']}}>
+                        {stat.toString()}
                     </Text>
-                </CenterView>
+                </View>
+                <Card.Title>
+                    <Text style={{fontSize: textSize[labelSize ?? 'small'], fontWeight: textWeight[labelWeight ?? 'normal']}}>{statName}</Text>
+                </Card.Title >
             </Card>
-        </View>
     );
 }
 
--- storybook/stories/StatBox/StatBox.stories.tsx
+++ storybook/stories/StatBox/StatBox.stories.tsx
@@ -7,46 +7,29 @@ import { View } from '../../../components/Themed';
 import CenterView from '../CenterView'; 
 import {FlatGrid} from 'react-native-super-grid'
 
-const stat = 12.45
+const stat = "12.45"
 const statName = "Speed"
-interface Stat {
+interface StatOptions {
     stat: string | number,
     statName: string
 }
 const stats = [{stat, statName}, {stat, statName},{stat, statName},{stat, statName}]
 storiesOf('StatBox', module)
   .add('with text', () => (
-      <StatBox statName={statName} stat={stat}/>
+      <StatBox statName={statName} stat={1.5}/>
   ))
   .add('multiple stats', () => (
       <View style={{flex: 1}}>
-          <StatBox statName={statName} stat={stat}/>
-          <StatBox statName={statName} stat={stat}/>
-          <StatBox statName={statName} stat={stat}/>
-          <StatBox statName={statName} stat={stat}/>
+          <StatBox statName={statName} stat={12.34} labelSize={"medium"}/>
+          <StatBox statName={statName} stat={stat} labelSize={"xlarge"}/>
+          <StatBox statName={statName} stat={stat} labelSize={"large"}/>
+          <StatBox statName={statName} stat={stat} labelSize={"small"}/>
       </View>
     ))
   .add('as grid', () => (
-    <CenterView>
-        <View style={{flex: 1,
-    flexGrow: 1,
-    flexDirection:'row',
-    alignItems: 'center',
-    borderColor: 'red',
-    borderWidth: 1}}>
-            <StatBox statName={statName} stat={stat}/>
-            <StatBox statName={statName} stat={stat}/>
-        </View>
-        <View style={{flex: 1, flexDirection: 'row'}}>
-            <StatBox statName={statName} stat={stat}/>
-            <StatBox statName={statName} stat={stat}/>
-        </View>
-    </CenterView>
-  ))
-  .add('as grid', () => (
     <FlatGrid
-        itemDimension={130}
+        itemDimension={175}
         data={stats}
-        renderItem={({ item }: { item: Stat }) => (<StatBox statName={item.statName} stat={item.stat}/>)}
+        renderItem={({ item }: { item: StatOptions }) => (<StatBox statName={item.statName} stat={item.stat}/>)}
     />
   ));
